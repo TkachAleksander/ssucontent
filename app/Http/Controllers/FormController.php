1e6db1d735d96c2cc7d10b7d9731c09a2aefd014
Tkach Aleksander <tkach.aleksandr.s@gmail.com>
2016-12-19 18:16:52 +0200

    Заполнение главных страниц информацией из бд

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -27,7 +27,9 @@ class FormController extends Controller
                 $forms =  DB::table('set_forms_users as sfu')
                     ->join('users as u', 'u.id','=','sfu.id_users')
                     ->join('forms as f', 'f.id','=','sfu.id_forms')
+                    ->join('status_checks as sc', 'sc.id','=','sfu.id_status_checks')
                     ->where('f.show','=', self::SHOW_FORMS)
+                    ->where('sfu.id_status_checks','=',2)
                     ->select('u.surname','u.name','u.middle_name','sfu.id_forms','f.name_forms')
                     ->get();
                 foreach ($forms as $key=>$form) {
@@ -37,15 +39,17 @@ class FormController extends Controller
                         ->select('se.name_set_elements', 'se.label_set_elements', 'sfe.width', 'e.name_elements')
                         ->get();
                 }
-
+                return view('homeAdmin',['forms' => $forms, 'role' => $role[0]->name_roles]);
             } else {
                 $role[0]->name_roles = null; // Не выводит имя пользователя в списке доступных форм (home)
 
                 $forms =  DB::table('set_forms_users as sfu')->where('sfu.id_users','=',$id_users)
                     ->join('users as u', 'u.id','=','sfu.id_users')
                     ->join('forms as f', 'f.id','=','sfu.id_forms')
+                    ->join('status_checks as sc', 'sc.id','=','sfu.id_status_checks')
                     ->where('f.show','=', self::SHOW_FORMS)
-                    ->select('u.surname','u.name','u.middle_name','sfu.id_forms','f.name_forms')
+                    ->select('u.surname','u.name','u.middle_name','sfu.id_forms','sc.name_status_checks','sc.border_color','f.name_forms')
+                    ->orderBy('sfu.id','asc')
                     ->get();
 
                 foreach ($forms as $key=>$form) {
@@ -56,13 +60,23 @@ class FormController extends Controller
                         ->get();
                 }
             }
-            return view('home',['forms' => $forms, 'role' => $role[0]->name_roles]);
+            return view('homeUser',['forms' => $forms, 'role' => $role[0]->name_roles]);
         } else {
             return view('home');
         }
     }
 
     public function submitFillForm(Request $request){
-        dd($request->all());
+        $id_set_forms_elements = DB::table('set_forms_elements')->where('id_forms','=',$request->input('id_form'))->pluck('id');
+
+        $row  = $i = 0;
+        foreach ($request->all() as $key=>$value){
+            if ($i++ >= 2) {
+                DB::table('values_forms')->insert(['id_set_forms_elements' => $id_set_forms_elements[$row++], 'value' => $value]);
+            }
+        }
+
+        DB::table('set_forms_users')->where('id_users','=',Auth::user()->id)->where('id_forms','=',$request->input('id_form'))
+        ->update(['id_status_checks'=>2]);
     }
 }
