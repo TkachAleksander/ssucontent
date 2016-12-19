2b2c6f80ee76755dec3e24346a96f33ba5181a74
Tkach Aleksander <tkach.aleksandr.s@gmail.com>
2016-12-19 18:12:10 +0200

    Главная страница администратора выводит формы отправленые на проверку перед публикацией

---
+++ resources/views/homeAdmin.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 ">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    @if (Auth::guest())
+                        Пожалуйста, авторизуйтесь для просмотра списка статей.
+                    @else
+                            Список форм на проверку:
+                    @endif
+                </div>
+                    <div class="panel-body">
+
+
+                        @if (isset($forms))
+                        <div class="panel-group" id="accordion">
+
+                        @foreach($forms as $form)
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#accordion{{$form->id_forms}}" class="forms-info collapsed" data-id="{{$form->id_forms}}">
+                                        {{ $form->name_forms }}
+                                        <span class="pull-right">{{ $form->surname.' '.$form->name }}</span>
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="accordion{{$form->id_forms}}" class="panel-collapse collapse ">
+                                <div class="panel-body">
+                                    <form action="submitFillForm" method="POST">
+                                        {{ csrf_field() }}
+
+                                        <input type="hidden" class="input-id-form" name="id_form" value="">
+
+                                        <div class="row">
+                                            <div class="col-sm-6">
+
+                                            </div>
+                                            <div id="content-form{{$form->id_forms}}" class="col-sm-6">
+
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <hr>
+                                                <input type="submit" class="btn btn-sm btn-success pull-right" value="Пртнять">
+                                            </div>
+                                        </div>
+
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+
+                        </div>
+                        @endif  
+                        
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
