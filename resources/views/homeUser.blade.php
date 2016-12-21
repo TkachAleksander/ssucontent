e1c03e9de97788a3a02454d5eb60755b771fde72
Tkach Aleksander <tkach.aleksandr.s@gmail.com>
2016-12-21 13:50:14 +0200

    Вывод формы по центру главная страница пользователя

--- resources/views/homeUser.blade.php
+++ resources/views/homeUser.blade.php
@@ -24,7 +24,8 @@
                                 <h4 class="panel-title">
                                     <a data-toggle="collapse" data-parent="#accordion" href="#accordion{{$form->id_forms}}" class="forms-info collapsed" data-id="{{$form->id_forms}}">
                                         {{ $form->name_forms }}
-                                            <span class="pull-right" style="color: {{$form->border_color}};">{{ $form->name_status_checks }}</span>
+                                        {{--<div style="width: 10px; height: 20px; background-color:{{$form->border_color}}; border-radius: 3px; display: inline-block;"></div>--}}
+                                        <span class="pull-right" style="color: {{$form->border_color}}; font-size: 13px">{{ $form->name_status_checks }}</span>
                                     </a>
                                 </h4>
                             </div>
@@ -36,13 +37,13 @@
                                         <input type="hidden" class="input-id-form" name="id_form" value="">
 
                                         <div class="row">
-                                            <div id="content-form{{$form->id_forms}}" class="col-sm-6">
+                                            <div id="content-form{{$form->id_forms}}" class="col-sm-offset-3 col-sm-6">
 
                                             </div>
                                         </div>
 
                                         <div class="row">
-                                            <div class="col-sm-6">
+                                            <div class="col-sm-offset-3 col-sm-6">
                                                 <hr>
                                                 <input type="submit" class="btn btn-sm btn-primary pull-right" value="Отправить">
                                             </div>
