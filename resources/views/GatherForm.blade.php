e622877de31c462c437c7b13213a411246f6b4d0
Tkach Aleksander <tkach.aleksandr.s@gmail.com>
2017-02-06 13:37:01 +0200

    php swith к полям старой формі добавлено свойство disabled

--- resources/views/GatherForm.blade.php
+++ resources/views/GatherForm.blade.php
@@ -11,7 +11,6 @@
 
         if (isset($forms_info[$key_fi]->enum_sub_elements)){
             foreach ($forms_info[$key_fi]->enum_sub_elements as $key_ese => $enum_sub_element){
-
                 $id_selected[$key_ese] = $enum_sub_element;
             }
         }
@@ -19,25 +18,27 @@
         $group = '<div class="'.$ver.'_group'.$id_fields_forms.' group">';
         $groupend = '</div>';
 
+        $disabled = ($ver == 'old') ? 'disabled' : '';
+
         switch ($form_info->name_elements){
             case 'input(text)':
                 echo $group;
                 echo $label;
-                echo '<input type="text" class="form-control" name="'.$id_fields_forms.'" value="'.$value.'"></input>';
+                echo '<input type="text" class="form-control" name="'.$id_fields_forms.'" value="'.$value.'" '.$disabled.'></input>';
                 echo $groupend;
                 break;
 
             case 'input(email)':
                 echo $group;
                 echo $label;
-                echo '<input type="email" class="form-control" name="'.$id_fields_forms.'" value="'.$value.'"></input>';
+                echo '<input type="email" class="form-control" name="'.$id_fields_forms.'" value="'.$value.'" '.$disabled.'></input>';
                 echo $groupend;
                 break;
 
             case 'textarea':
                 echo $group;
                 echo $label;
-                echo '<textarea class="form-control textarea-resize-none" name="'.$id_fields_forms.'">'.$value.'</textarea>';
+                echo '<textarea class="form-control textarea-resize-none" name="'.$id_fields_forms.'" '.$disabled.'>'.$value.'</textarea>';
                 echo $groupend;
                 break;
 
@@ -53,7 +54,7 @@
                             }
                         }
                             }
-                        echo '<label><input type="checkbox" name="'.$id_fields_forms.'[]" value="'.$id_sub_element.'"'.$checked.'> '.$labels_sub_elements[$key_ise].'</label><br> ';
+                        echo '<label><input type="checkbox" name="'.$id_fields_forms.'[]" value="'.$id_sub_element.'"'.$checked.' '.$disabled.'> '.$labels_sub_elements[$key_ise].'</label><br> ';
                     }
                 echo $groupend;
                 break;
@@ -70,14 +71,14 @@
                             }
                         }
                             }
-                        echo '<label><input type="radio" name="'.$id_fields_forms.'[]" value="'.$id_sub_element.'"'.$checked.'> '.$labels_sub_elements[$key_ise].'</label><br> ';
+                        echo '<label><input type="radio" name="'.$id_fields_forms.'[]" value="'.$id_sub_element.'"'.$checked.' '.$disabled.'> '.$labels_sub_elements[$key_ise].'</label><br> ';
                     }
                 echo $groupend;
                 break;
 
             case 'select':
                 echo $group;
-                echo $label.'<select name="'.$id_fields_forms.'[]" class="margin-left-10 multiselect">';
+                echo $label.'<select name="'.$id_fields_forms.'[]" class="margin-left-10 multiselect" '.$disabled.'>';
                     foreach ($id_sub_elements as $key_ise => $id_sub_element){
                         $checked = '';
                         if (isset($id_selected)){
